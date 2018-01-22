c83ae671d9ba73e2e92fa1a5f3b80ea3c51b6cc7
Behtsa <monze_gdbm@hotmail.com>
2018-01-22 00:01:13 -0600

    added data.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+ $(document).ready(init);
 /*function for the splash view
 using setTimeOut*/
 $(function(){
@@ -9,9 +10,36 @@ $(function(){
 /*google map**/
 function myMap() {
     var mapOptions = {
-        center: new google.maps.LatLng(19.432608, -99.133208),
-        zoom: 10,
+        center: new google.maps.LatLng(19.4203024, -99.1631142),
+        zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+
+/*si escriben mexicana debe mostrar resaurantes de commex
+1.- se debe guardar el valor del inout
+2. se debe comparar con un obj del data*/
+var $type = $("#type");
+var $search = $("#search");
+
+function init() {
+	$search.click(getType);
+}
+
+function getType() {
+	var $value = $type.val();
+	var $restaurants = data[$value]['restaurants'];
+	printRestaurants($restaurants)
+}
+
+function printRestaurants($restaurants) {
+	/*iterate through the array to show the images*/
+	console.log($restaurants[0].photo);
+	$("#first").attr("src", $restaurants[0].photo);
+}
+
+
+
+
+
---
+++ js/data.js
@@ -0,0 +1,47 @@
+var data = {
+	'mexicana': {
+		'restaurants': [
+			{'name': 'EL MEXICANO',
+				'lat': 19.4287161,
+				'long': -99.16875270000003,
+				'phrase': 'Restaurante y mezcaleria',
+				'address': 'Eje 2 Poniente (Río Tiber) 107, Cuauhtémoc, 06500 Ciudad de México, CDMX',
+				'rates': '$100 a $370',
+				'photo': 'assets/images/mexicano.jpg'
+			},
+			{
+				'name': 'AZUL CONDESA',
+				'lat': 19.4127607,
+				'long': -99.17152959999999,
+				'phrase': 'Platos mexicanos de autor en un espacio informal y sofisticado con terraza y decoraciones de madera.',
+				'address': 'Av Nuevo León 68, Hipódromo Condesa, 06100 Ciudad de México, CDMX',
+				'rates': '$150 a $250',
+				'photo': 'assets/images/azul.jpg'	
+			},
+			{'name': 'LA TECLA',
+				'lat': 19.4198427,
+				'long': -99.16552939999997,
+				'phrase': 'Casona clásica con cocina mexicana contemporánea servida en un acogedor comedor con estilo ecléctico.',
+				'address': 'Calle de Durango 186, Roma Nte., 06700 Ciudad de México, CDMX',
+				'rates': '$200 a $400',
+				'photo': 'assets/images/tecla.jpeg'
+			},
+			{'name': 'EL BESO HUASTECO',
+				'lat': 19.416649,
+				'long': -99.15788550000002,
+				'phrase': 'Restaurante en una casona decorada con artesanías que sirve especialidades huastecas en una atmósfera casera.',
+				'address': 'Córdoba 146, Roma Nte., 06700 Ciudad de México, CDMX',
+				'rates': '$100 a $370',
+				'photo': 'assets/images/beso.jpg'
+			},
+			{'name': 'EL MEXICANO',
+				'lat': 19.4287161,
+				'long': -99.16875270000003,
+				'phrase': 'Restaurante y mezcaleria',
+				'address': 'Eje 2 Poniente (Río Tiber) 107, Cuauhtémoc, 06500 Ciudad de México, CDMX',
+				'rates': '$100 a $370',
+				'photo': 'assets/images/mexicano.jpg'
+			}
+		]
+	}
+}
