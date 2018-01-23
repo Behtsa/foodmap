81a63726a25b54cad7a4c6757aa86221eeebb574
Behtsa <monze_gdbm@hotmail.com>
2018-01-22 18:29:31 -0600

    modals dinamically

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
- $(document).ready(init);
+$(document).ready(init);
 /*function for the splash view
 using setTimeOut*/
 $(function(){
@@ -18,43 +18,71 @@ var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
 
 /*adding functionality on the general page*/
-var $type = $("#type");
-var $search = $("#search");
+// var $type = $("#type");
+// var $search = $("#search");
+ var dataRestaurants = data['restaurants'];
 
 /*this function toggles the event of the search button*/
 function init() {
-	$search.click(getType);
+	for (var i = 0; i < dataRestaurants.length; i++) {
+		showImage(dataRestaurants[i]);
+	}
+	// $search.click(getType);
+}
+
+function showImage(dataRes) {
+	// console.log(dataRes);
+	var $container = $("#img_food");
+	var $newLink = $("<a />", {'class': "col-xs-4 btn"}); //col-xs-offset-1
+	var $newImg = $("<img>", {'class': "img-responsive"});
+	$newLink.attr("href", "#modal1");
+	$newLink.attr("data-toggle", "modal");
+	$newLink.attr("data-name", dataRes.name);
+	$newImg.attr("src", dataRes.photo);
+	$newLink.append($newImg);
+	$container.append($newLink);
+	$newLink.click(function(){
+		modal(dataRes);
+	})
+}
+
+function modal(dataRes) {
+	console.log(dataRes);
+	$("#name").text(dataRes.name);
+	$("#phrase").text(dataRes.phrase);
+	$("#address").text(dataRes.address);
+	$("#rates").text(dataRes.rates);
 }
 
 /*this function filter retaurnts per type*/
-function getType() {
-	var $value = $type.val();
-	var $restaurants = data[$value]['restaurants'];
-	printRestaurants($restaurants)
-}
+// function getType() {
+// 	var $value = $type.val();
+// 	var $restaurants = data[$value]['restaurants'];
+// 	printRestaurants($restaurants)
+// }
 
-function printRestaurants($restaurants) {
-	var $restaurant = $restaurants;
-	/*iterate through the array to show the images*/
-	// console.log($ids[0]);
-	// console.log($restaurants[0].photo);
-	// $($ids[0]).attr("src", $restaurants[0].photo);
-	var $ids = ["#first", "#second", "#third", "#fourth", "#fifth"];
-	for(var i= 0; i < $ids.length; i++) {
-		$($ids[i]).attr("src", $restaurants[i].photo);
-	}
-	printModalInfo($restaurant);
-}
+// function printRestaurants($restaurants) {
+// 	var $restaurant = $restaurants;
+// 	/*iterate through the array to show the images*/
+// 	// console.log($ids[0]);
+// 	// console.log($restaurants[0].photo);
+// 	// $($ids[0]).attr("src", $restaurants[0].photo);
+// 	var $ids = ["#first", "#second", "#third", "#fourth", "#fifth"];
+// 	for(var i= 0; i < $ids.length; i++) {
+// 		$($ids[i]).attr("src", $restaurants[i].photo);
+// 	}
+// 	printModalInfo($restaurant);
+// }
 
-function printModalInfo($restaurants) {
-	$('a').click(function() {
-		console.log($restaurants[0].name);
-		$("#name").html($restaurants[0].name);
-		$("#phrase").html($restaurants[0].phrase);
-		$("#address").html($restaurants[0].address);
-		$("#rates").html($restaurants[0].rates);
-	})
-}
+// function printModalInfo($restaurants) {
+// 	$('a').click(function() {
+// 		console.log($restaurants[0].name);
+// 		$("#name").html($restaurants[0].name);
+// 		$("#phrase").html($restaurants[0].phrase);
+// 		$("#address").html($restaurants[0].address);
+// 		$("#rates").html($restaurants[0].rates);
+// 	})
+// }
 
 
 
--- js/data.js
+++ js/data.js
@@ -7,7 +7,8 @@ var data = {
 			'phrase': 'Restaurante y mezcaleria',
 			'address': 'Eje 2 Poniente (Río Tiber) 107, Cuauhtémoc, 06500 Ciudad de México, CDMX',
 			'rates': '$100 a $370',
-			'photo': 'assets/images/mexicano.jpg'
+			'photo': 'assets/images/mexicano.jpg',
+			'type': 'mexicana'
 		},
 		{
 			'name': 'AZUL CONDESA',
@@ -16,7 +17,8 @@ var data = {
 			'phrase': 'Platos mexicanos de autor en un espacio informal y sofisticado con terraza y decoraciones de madera.',
 			'address': 'Av Nuevo León 68, Hipódromo Condesa, 06100 Ciudad de México, CDMX',
 			'rates': '$150 a $250',
-			'photo': 'assets/images/azul.jpg'	
+			'photo': 'assets/images/azul.jpg',
+			'type': 'mexicana'	
 		},
 		{	
 			'name': 'LA TECLA',
@@ -25,7 +27,8 @@ var data = {
 			'phrase': 'Casona clásica con cocina mexicana contemporánea servida en un acogedor comedor con estilo ecléctico.',
 			'address': 'Calle de Durango 186, Roma Nte., 06700 Ciudad de México, CDMX',
 			'rates': '$200 a $400',
-			'photo': 'assets/images/tecla.jpg'
+			'photo': 'assets/images/tecla.jpg',
+			'type': 'mexicana'
 		},
 		{	
 			'name': 'EL BESO HUASTECO',
@@ -34,16 +37,17 @@ var data = {
 			'phrase': 'Restaurante en una casona decorada con artesanías que sirve especialidades huastecas en una atmósfera casera.',
 			'address': 'Córdoba 146, Roma Nte., 06700 Ciudad de México, CDMX',
 			'rates': '$100 a $370',
-			'photo': 'assets/images/beso.jpg'
+			'photo': 'assets/images/beso.jpg',
+			'type': 'mexicana'
 		},
 		{	
-			'name': 'EL MEXICANO',
-			'lat': 19.4287161,
-			'long': -99.16875270000003,
-			'phrase': 'Restaurante y mezcaleria',
-			'address': 'Eje 2 Poniente (Río Tiber) 107, Cuauhtémoc, 06500 Ciudad de México, CDMX',
-			'rates': '$100 a $370',
-			'photo': 'assets/images/mexicano.jpg'
+			'name': 'PARRILLA ARGENTINA BLAH BLAH',
+			'lat': 19.4246969,
+			'long': -99.1663122,
+			'phrase': 'Cortes Argentinos',
+			'address': 'Florencia 44, Juárez, 06600 Ciudad de México, CDMX',
+			'rates': '$150 a $250',
+			'photo': 'assets/images/blah-blah.jpg'
 		}
 	]
 }
